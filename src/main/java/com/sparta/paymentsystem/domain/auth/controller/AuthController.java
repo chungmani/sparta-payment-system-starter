0d5a1ac8c256ff86ca9caf68804050df8f071b06
chungmani <cndaks19@naver.com>
2026-08-14 11:51:45 +0900

    feat(Member): Member 엔티티 구현

---
+++ src/main/java/com/sparta/paymentsystem/domain/auth/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.sparta.paymentsystem.domain.auth.controller;
+
+import com.sparta.paymentsystem.domain.auth.dto.AuthResponse;
+import com.sparta.paymentsystem.domain.auth.dto.LoginRequest;
+import com.sparta.paymentsystem.domain.auth.dto.SignupRequest;
+import com.sparta.paymentsystem.domain.auth.service.AuthService;
+import com.sparta.paymentsystem.domain.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@Valid @RequestBody SignupRequest request) {
+        authService.signup(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
