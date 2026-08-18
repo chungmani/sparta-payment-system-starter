534fece9b6540dd5849a8a16ae3d1910bedf2c59
chungmani <cndaks19@naver.com>
2026-08-18 22:45:30 +0900

    feat(order): 주문 API 엔드포인트 추가

--- src/main/java/com/sparta/paymentsystem/domain/order/controller/OrderController.java
+++ src/main/java/com/sparta/paymentsystem/domain/order/controller/OrderController.java
@@ -1,21 +1,22 @@
 package com.sparta.paymentsystem.domain.order.controller;
 
 import com.sparta.paymentsystem.domain.order.dto.CheckoutResponse;
+import com.sparta.paymentsystem.domain.order.dto.OrderCheckoutRequest;
+import com.sparta.paymentsystem.domain.order.dto.OrderCheckoutResponse;
+import com.sparta.paymentsystem.domain.order.dto.OrderResponse;
 import com.sparta.paymentsystem.domain.order.facade.OrderFacade;
 import com.sparta.paymentsystem.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class OrderController {
 
     private final OrderFacade orderFacade;
@@ -23,8 +24,28 @@ public class OrderController {
     @GetMapping("/checkout")
     public ResponseEntity<ApiResponse<CheckoutResponse>> checkout(
             @AuthenticationPrincipal Long memberId,
-            @RequestParam(required = false)List<Long> cartItemIds
-            ) {
+            @RequestParam(required = false) List<Long> cartItemIds
+    ) {
         return ResponseEntity.ok(ApiResponse.ok(orderFacade.getCheckout(memberId, cartItemIds)));
     }
+
+    @PostMapping("/checkout")
+    public ResponseEntity<ApiResponse<OrderCheckoutResponse>> createOrder(
+            @AuthenticationPrincipal Long memberId,
+            @RequestBody(required = false) OrderCheckoutRequest request) {
+        OrderCheckoutResponse response = orderFacade.createOrder(memberId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.ok(response));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<OrderResponse>>> getOrders(@AuthenticationPrincipal Long memberId) {
+        return ResponseEntity.ok(ApiResponse.ok(orderFacade.getOrders(memberId)));
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<ApiResponse<OrderResponse>> getOrder(@AuthenticationPrincipal Long memberId,
+                                                               @PathVariable Long orderId) {
+        return ResponseEntity.ok(ApiResponse.ok(orderFacade.getOrder(memberId, orderId)));
+    }
+
 }
